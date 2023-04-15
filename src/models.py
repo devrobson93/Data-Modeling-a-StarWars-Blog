6b8ee09b5e9de1a47fbaf5f863edd3dd64c7dcd7
devrobson93 <robson.dev93@gmail.com>
2023-04-15 17:12:51 +0000

    data modeling starwars

--- src/models.py
+++ src/models.py
@@ -7,26 +7,62 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+#     def to_dict(self):
+#         return {}
+
+class User (Base):
+    __tablename__ = "user"
+    id = Column (Integer, primary_key=True)
+    name = Column(String)      
+
+class Favorites (Base):
+    __tablename__ = "favorites"
+    id = id = Column (Integer, primary_key=True)
+    user_id = Column (Integer, ForeignKey("user.id"))
+    basic_data_id = Column(Integer, ForeignKey("basic_data.id"))
+
+class Basic_data (Base):
+    __tablename__ = "basic_data"
+    id = Column (Integer, primary_key=True)
+    name = Column(String)
+    description = Column (String)
+
+class Character (Base):
+    __tablename__ = "character"
+    gender = Column(String)
+    hair_color = Column(String)
+    eye_color = Column(String)
+    basic_data_id = Column(Integer, ForeignKey("basic_data.id"),primary_key=True)
+
+class Planet (Base):
+    __tablename__ = "planet"
+    population = Column(Integer)
+    terrain = Column(String)
+    climate = Column (String)
+    orbital_period = Column(Integer)
+    rotation_period = Column (Integer)
+    diameter = Column(Integer)
+    basic_data_id = Column(Integer, ForeignKey("basic_data.id"),primary_key=True)
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
